80d8c7c50917b27c8d923dff0313511c553225c6
Emerson Estrella <emerson.estrella@gmail.com>
2011-08-18 02:46:43 -0300

    - import fixes - jquerymobile

--- apps/frontend/modules/campeonatos/actions/actions.class.php
+++ apps/frontend/modules/campeonatos/actions/actions.class.php
@@ -45,182 +45,178 @@ class campeonatosActions extends sfActions
     //echo var_dump($array["campeonatos"]);
     foreach($array["campeonatos"] as $a){
       echo "<pre>";
-      if(!$tournament = Doctrine_Core::getTable('Tournament')->findOneBySlug($a["edicao"]["slug"])){
-        echo "Adding Tournament: ".$a["nome"]."...\n";
+
+      echo "Adding Tournament: ".$a["nome"]."...\n";
+      if(!$tournament = Doctrine_Core::getTable('Tournament')->findOneBySlug($a["slug"]))
         $tournament = new Tournament();
-        $tournament->setName($a["nome"]);
-        $tournament->setSlug($a["slug"]);
-        if(isset($a["regulamento"]))
-          $tournament->setRules($a["regulamento"]);
-        $tournament->save();
-        //echo "nome: ".$a["nome"]."\n";
-        //echo "slug: ".$a["slug"]."\n";
-        foreach($a as $k=>$v){
-          if(is_array($v)){
+      $tournament->setName($a["nome"]);
+      $tournament->setSlug($a["slug"]);
+      if(isset($a["regulamento"]))
+        $tournament->setRules($a["regulamento"]);
+
+      $tournament->save();
+      //echo "nome: ".$a["nome"]."\n";
+      //echo "slug: ".$a["slug"]."\n";
+
+      foreach($a as $k=>$v){
+        if(is_array($v)){
+          if(!$tournamentEdition = Doctrine_Core::getTable('TournamentEdition')->findOneBySlug($v["slug"]))
             $tournamentEdition = new TournamentEdition();
-            $tournamentEdition->setTournamentId($tournament->getId());
-            $tournamentEdition->setName($v["nome"]);
-            $tournamentEdition->setSlug($v["slug"]);
-            $tournamentEdition->save();
-            //echo "edicao-nome: ".$v["nome"]."\n";
-            //echo "edicao-slug: ".$v["slug"]."\n";
+          $tournamentEdition->setTournamentId($tournament->getId());
+          $tournamentEdition->setName($v["nome"]);
+          $tournamentEdition->setSlug($v["slug"]);
+          $tournamentEdition->save();
+          //echo "edicao-nome: ".$v["nome"]."\n";
+          //echo "edicao-slug: ".$v["slug"]."\n";
+        }
+      }
+
+      if($tournament->getSlug()=="turco")
+        $contents = file_get_contents('http://globoesporte.globo.com/dynamo/futebol/campeonato/turco/campeonatoturco2011/classificacao.json');
+      else
+        $contents = file_get_contents('http://globoesporte.globo.com/dynamo/futebol/campeonato/'.$tournament->getSlug().'/'.$tournamentEdition->getSlug().'/classificacao.json');
+      $array2 = json_decode($contents, true);
+
+      //TEAMS
+      foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["equipes"] as $key=>$value){
+        if(!$team = Doctrine_Core::getTable('Team')->findOneByTeamExtId($key))
+          $team = new Team();
+        echo "Adding Team: ".$value["nome"]."...\n";
+        $team = new Team();
+        $team->setTeamExtId($key);
+        $team->setOfficialName($value["nome"]);
+        $team->setName($value["nome_popular"]);
+        $team->setNickname($value["apelido"]);
+        $team->setInitials($value["sigla"]);
+        #TEAM-LOGO
+        $aux = @end(explode("/",$value["escudo"]));
+        $img = @sfConfig::get('sf_upload_dir').'/assets/teams/'.$aux;
+        @file_put_contents($img, file_get_contents($value["escudo"]));
+        $team->setLogo($aux);
+        $team->save();
+        //echo var_dump($a);
+      }
+
+      //STADIUMS
+      foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["sedes"] as $key=>$value){
+        if(!$stadium = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($key))
+          $stadium = new Stadium();
+        echo "Adding Stadium: ".$value["nome"]."...\n";
+        $stadium->setStadiumExtId($key);
+        $stadium->setOfficialName($value["nome"]);
+        $stadium->setName($value["nome_popular"]);
+        $stadium->setGeoLocation($value["localizacao_geografica"]);
+        $stadium->setLocation($value["localizacao"]);
+        $stadium->setInauguration($value["inauguracao"]);
+        $stadium->setCapacity($value["capacidade_maxima"]);
+        $stadium->setAddress($value["endereco"]);
+        $stadium->save();
+        //echo var_dump($value);
+      }
+
+      //GAMES
+      foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["fases"] as $key=>$value){
+        if(count($value["jogos"])>0){
+          foreach($value["jogos"] as $k=>$v){
+            if(!$game = Doctrine_Core::getTable('Game')->findOneByGameExtId($v["jogo_id"]))
+              $game = new Game();
+            echo "Adding Game1 : ".$v["jogo_id"]."...\n";
+            $d = explode("-", $v["data_original"]);
+            if(!isset($d[2]))
+              $d = explode("/", $v["data_original"]);
+            $h = explode("h", $v["hora"]);
+            if(!isset($h[1]))
+              $h = explode(":", $v["hora"]);
+            if(!isset($h[1]))
+              $h = explode(":", "00:00");
+            if($d[1] < 10)
+              $d[1] = "0".$d[1];
+            if($d[0] < 10)
+              $d[0] = "0".$d[0];
+            //echo "\tdate: ".$v["data_original"]." ".$v["data_original"]."\n";
+            //echo "\tdate: ".$d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1].":"."00"."\n";
+            $game->setGameExtId($v["jogo_id"]);
+            $game->setTournamentId($tournament->getId());
+            $game->setDateStart($d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1].":"."00");
+            $game->setDate($v["data"]);
+            $game->setTime($v["hora"]);
+            $game->setRound($v["rodada"]);
+            if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_mandante"]))
+              $game->setHomeTeamId($t->getId());
+            if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_visitante"]))
+              $game->setAwayTeamId($t->getId());
+            $game->setHomeTeamExtId($v["equipe_mandante"]);
+            $game->setAwayTeamExtId($v["equipe_visitante"]);
+            $game->setHomeTeamLogo($v["escudo_mandante"]);
+            $game->setAwayTeamLogo($v["escudo_visitante"]);
+            $game->setHomeTeamName($v["nome_popular_mandante"]);
+            $game->setAwayTeamName($v["nome_popular_visitante"]);
+            $game->setHomeTeamInitials($v["sigla_mandante"]);
+            $game->setAwayTeamInitials($v["sigla_visitante"]);
+            $game->setHomeTeamSlug($v["slug_mandante"]);
+            $game->setAwayTeamSlug($v["slug_visitante"]);
+            $game->setHomeTeamScore($v["placar_mandante"]);
+            $game->setAwayTeamScore($v["placar_visitante"]);
+            $game->setHomeTeamPenaltyScore($v["placar_penalti_mandante"]);
+            $game->setAwayTeamPenaltyScore($v["placar_penalti_visitante"]);
+            if($v["sede"]>0){
+              if($s = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($v["sede"]))
+                $game->setStadiumId($s->getId());
+            }
+            $game->setStadiumExtId($v["sede"]);
+            $game->setStadiumName($v["local_jogo"]);
+            $game->setUrl($v["url_confronto"]);
+            $game->save();
+          }
+        }
+        elseif(count($value["chaves"])>0){
+          foreach($value["chaves"] as $k=>$v){
+            if($v["ida_jogo"]["jogo_id"]>0)
+              $data[] = $v["ida_jogo"];
+            if($v["volta_jogo"]["jogo_id"]>0)
+              $data[] = $v["volta_jogo"];
+          }
+          foreach($data as $k=>$v){
+            if(!$game = Doctrine_Core::getTable('Game')->findOneByGameExtId($v["jogo_id"]))
+              $game = new Game();
+            echo "Adding Game2 : ".$v["jogo_id"]."...\n";
+            $d1 = explode(" ", $v["data"]);
+            $d = explode("/", $d1[1]);
+            if(!isset($d[2]))
+              $d = explode("-", $d1[1]);
+            $h = explode("h", $v["hora"]);
+            $game->setGameExtId($v["jogo_id"]);
+            $game->setTournamentId($tournament->getId());
+            $game->setDateStart($d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1]."00");
+            $game->setDate($v["data"]);
+            $game->setTime($v["hora"]);
+            $game->setRound($v["rodada"]);
+            if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_mandante_id"]))
+              $game->setHomeTeamId($t->getId());
+            if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_visitante_id"]))
+              $game->setAwayTeamId($t->getId());
+            $game->setHomeTeamExtId($v["equipe_mandante_id"]);
+            $game->setAwayTeamExtId($v["equipe_visitante_id"]);
+            $game->setHomeTeamScore($v["placar_mandante"]);
+            $game->setAwayTeamScore($v["placar_visitante"]);
+            $game->setHomeTeamPenaltyScore($v["placar_penalti_mandante"]);
+            $game->setAwayTeamPenaltyScore($v["placar_penalti_visitante"]);
+            if($v["sede"]>0){
+              if($s = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($v["sede"]))
+                $game->setStadiumId($s->getId());
+            }
+            $game->setStadiumExtId($v["sede"]);
+            $game->setUrl($v["url_confronto"]);
+            $game->save();                
+          }
+        }
+        else{
+          echo "WTF!?";
+          foreach($value as $k=>$v){
+            var_dump($v);
           }
         }
 
-
-        if($tournament->getSlug()=="turco")
-          $contents = file_get_contents('http://globoesporte.globo.com/dynamo/futebol/campeonato/turco/campeonatoturco2011/classificacao.json');
-        else
-          $contents = file_get_contents('http://globoesporte.globo.com/dynamo/futebol/campeonato/'.$tournament->getSlug().'/'.$tournamentEdition->getSlug().'/classificacao.json');
-        $array2 = json_decode($contents, true);
-
-        //TEAMS
-        foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["equipes"] as $key=>$value){
-          if(!$team = Doctrine_Core::getTable('Team')->findOneBySlug($value["slug"])){
-            echo "Adding Team: ".$value["nome"]."...\n";
-            $team = new Team();
-            $team->setTeamExtId($key);
-            $team->setOfficialName($value["nome"]);
-            $team->setName($value["nome_popular"]);
-            $team->setNickname($value["apelido"]);
-            $team->setLogo($value["escudo"]);
-            $team->setInitials($value["sigla"]);
-            
-            #TEAM-LOGO
-            $aux = @end(explode("/",$value["escudo"]));
-            $img = @sfConfig::get('sf_upload_dir').'/assets/teams/'.$aux;
-            @file_put_contents($img, file_get_contents($value["escudo"]));
-
-            $team->save();
-            //echo var_dump($a);
-          }
-        }
-
-        //STADIUMS
-        foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["sedes"] as $key=>$value){
-          if(!$stadium = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($key)){
-            echo "Adding Stadium: ".$value["nome"]."...\n";
-            $stadium = new Stadium();
-            $stadium->setStadiumExtId($key);
-            $stadium->setOfficialName($value["nome"]);
-            $stadium->setName($value["nome_popular"]);
-            $stadium->setGeoLocation($value["localizacao_geografica"]);
-            $stadium->setLocation($value["localizacao"]);
-            $stadium->setInauguration($value["inauguracao"]);
-            $stadium->setCapacity($value["capacidade_maxima"]);
-            $stadium->setAddress($value["endereco"]);
-            $stadium->save();
-            //echo var_dump($value);
-          }
-        }
-
-        //GAMES
-        foreach($array2["lista_de_jogos"]["campeonato"]["edicao_campeonato"]["fases"] as $key=>$value){
-          if(count($value["jogos"])>0){
-            foreach($value["jogos"] as $k=>$v){
-              if(!$game = Doctrine_Core::getTable('Game')->findOneByGameExtId($v["jogo_id"])){
-                echo "Adding Game1 : ".$v["jogo_id"]."...\n";
-                $d = explode("-", $v["data_original"]);
-                if(!isset($d[2]))
-                  $d = explode("/", $v["data_original"]);
-                $h = explode("h", $v["hora"]);
-                if(!isset($h[1]))
-                  $h = explode(":", $v["hora"]);
-                if(!isset($h[1]))
-                  $h = explode(":", "00:00");
-                if($d[1] < 10)
-                  $d[1] = "0".$d[1];
-                if($d[0] < 10)
-                  $d[0] = "0".$d[0];
-                //echo "\tdate: ".$v["data_original"]." ".$v["data_original"]."\n";
-                //echo "\tdate: ".$d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1].":"."00"."\n";
-                $game = new Game();
-                $game->setGameExtId($v["jogo_id"]);
-                $game->setTournamentId($tournament->getId());
-                $game->setDateStart($d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1].":"."00");
-                $game->setDate($v["data"]);
-                $game->setTime($v["hora"]);
-                $game->setRound($v["rodada"]);
-                if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_mandante"]))
-                  $game->setHomeTeamId($t->getId());
-                if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_visitante"]))
-                  $game->setAwayTeamId($t->getId());
-                $game->setHomeTeamExtId($v["equipe_mandante"]);
-                $game->setAwayTeamExtId($v["equipe_visitante"]);
-                $game->setHomeTeamLogo($v["escudo_mandante"]);
-                $game->setAwayTeamLogo($v["escudo_visitante"]);
-                $game->setHomeTeamName($v["nome_popular_mandante"]);
-                $game->setAwayTeamName($v["nome_popular_visitante"]);
-                $game->setHomeTeamInitials($v["sigla_mandante"]);
-                $game->setAwayTeamInitials($v["sigla_visitante"]);
-                $game->setHomeTeamSlug($v["slug_mandante"]);
-                $game->setAwayTeamSlug($v["slug_visitante"]);
-                $game->setHomeTeamScore($v["placar_mandante"]);
-                $game->setAwayTeamScore($v["placar_visitante"]);
-                $game->setHomeTeamPenaltyScore($v["placar_penalti_mandante"]);
-                $game->setAwayTeamPenaltyScore($v["placar_penalti_visitante"]);
-                if($v["sede"]>0){
-                  if($s = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($v["sede"]))
-                    $game->setStadiumId($s->getId());
-                }
-                $game->setStadiumExtId($v["sede"]);
-                $game->setStadiumName($v["local_jogo"]);
-                $game->setUrl($v["url_confronto"]);
-                $game->save();
-              }
-            }
-          }
-          elseif(count($value["chaves"])>0){
-            foreach($value["chaves"] as $k=>$v){
-              if($v["ida_jogo"]["jogo_id"]>0)
-                $data[] = $v["ida_jogo"];
-              if($v["volta_jogo"]["jogo_id"]>0)
-                $data[] = $v["volta_jogo"];
-            }
-            foreach($data as $k=>$v){
-              if(!$game = Doctrine_Core::getTable('Game')->findOneByGameExtId($v["jogo_id"])){
-                echo "Adding Game2 : ".$v["jogo_id"]."...\n";
-                $d1 = explode(" ", $v["data"]);
-                $d = explode("/", $d1[1]);
-                if(!isset($d[2]))
-                  $d = explode("-", $d1[1]);
-                $h = explode("h", $v["hora"]);
-                $game = new Game();
-                $game->setGameExtId($v["jogo_id"]);
-                $game->setTournamentId($tournament->getId());
-                $game->setDateStart($d[2]."-".$d[1]."-".$d[0]." ".$h[0].":".$h[1]."00");
-                $game->setDate($v["data"]);
-                $game->setTime($v["hora"]);
-                $game->setRound($v["rodada"]);
-                if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_mandante_id"]))
-                  $game->setHomeTeamId($t->getId());
-                if($t = Doctrine_Core::getTable('Team')->findOneByTeamExtId($v["equipe_visitante_id"]))
-                  $game->setAwayTeamId($t->getId());
-                $game->setHomeTeamExtId($v["equipe_mandante_id"]);
-                $game->setAwayTeamExtId($v["equipe_visitante_id"]);
-                $game->setHomeTeamScore($v["placar_mandante"]);
-                $game->setAwayTeamScore($v["placar_visitante"]);
-                $game->setHomeTeamPenaltyScore($v["placar_penalti_mandante"]);
-                $game->setAwayTeamPenaltyScore($v["placar_penalti_visitante"]);
-                if($v["sede"]>0){
-                  if($s = Doctrine_Core::getTable('Stadium')->findOneByStadiumExtId($v["sede"]))
-                    $game->setStadiumId($s->getId());
-                }
-                $game->setStadiumExtId($v["sede"]);
-                $game->setUrl($v["url_confronto"]);
-                $game->save();                
-              }
-            }
-          }
-          else{
-            echo "WTF!?";
-            foreach($value as $k=>$v){
-              var_dump($v);
-            }
-          }
-        }
-      }else{
-        echo "Skiping: ".$a["nome"]."...\n";
       }
       echo "</pre>";
     }
--- apps/frontend/modules/webapp/templates/indexSuccess.php
+++ apps/frontend/modules/webapp/templates/indexSuccess.php
@@ -1,113 +1,149 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
-    <title>Tabs 2</title>
-  <link rel="stylesheet" href="/js/sencha-touch-1.1.0/resources/css/sencha-touch.css" type="text/css">
-  <script type="text/javascript" src="/js/sencha-touch-1.1.0/sencha-touch-debug.js"></script>    
+<!DOCTYPE html> 
+<html> 
+<head> 
+  <meta charset="utf-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <title>jQuery Mobile: Demos and Documentation</title> 
+  <link rel="stylesheet"  href="/js/jquery.mobile/jquery.mobile-1.0b2.min.css" /> 
+  <link rel="stylesheet" href="/css/webapp.css" /> 
+  <script src="/js/jquery-1.6.2.min.js"></script> 
+  <script src="/js/webapp.js"></script> 
+  <script src="/js/jquery.mobile/jquery.mobile-1.0b2.min.js"></script>
+</head> 
+<body> 
+  
+<div data-role="page" id="jqm-home" class="type-home"> 
+  <div data-role="content"> 
+    <div class="content-secondary">
+      <div id="jqm-homeheader"> 
+        <h1 id="jqm-logo"><img src="docs/_assets/images/jquery-logo.png" alt="jQuery Mobile Framework" /></h1> 
+        <p>A Touch-Optimized Web Framework for Smartphones &amp; Tablets</p> 
+        <p id="jqm-version">Beta Release</p> 
+      </div> 
+  
+      <p class="intro"><strong>Welcome.</strong> Browse the jQuery Mobile components and learn how to make rich, accessible, touch-friendly websites and apps.</p> 
+      
+      <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="f"> 
+        <li data-role="list-divider">Overview</li> 
+        <li><a href="docs/about/intro.html">Intro to jQuery Mobile</a></li> 
+        <li><a href="docs/about/features.html">Features</a></li> 
+        <li><a href="docs/about/accessibility.html">Accessibility</a></li> 
+        <li><a href="docs/about/platforms.html">Supported platforms</a></li> 
+      </ul> 
+      
+    <div class="ui-body ui-body-b"> 
+ 
+      <div data-role="fieldcontain"> 
+           <label for="name-b">Text Input:</label> 
+           <input type="text" name="name" id="name-b" value=""  /> 
+      </div> 
+ 
+      <div data-role="fieldcontain"> 
+        <label for="switch-b">Flip switch:</label> 
+        <select name="switch-b" id="switch-b" data-role="slider"> 
+          <option value="off">Off</option> 
+          <option value="on">On</option> 
+        </select> 
+      </div> 
+ 
+      <div data-role="fieldcontain"> 
+        <label for="slider-b">Slider:</label> 
+        <input type="range" name="slider" id="slider-b" value="0" min="0" max="100"  /> 
+      </div> 
+ 
+ 
+      <div data-role="fieldcontain"> 
+      <fieldset data-role="controlgroup" data-type="horizontal"> 
+          <legend>Font styling:</legend> 
+          <input type="checkbox" name="checkbox-6b" id="checkbox-6b" class="custom" /> 
+        <label for="checkbox-6b">b</label> 
+ 
+        <input type="checkbox" name="checkbox-7b" id="checkbox-7b" class="custom" /> 
+        <label for="checkbox-7b"><em>i</em></label> 
+ 
+        <input type="checkbox" name="checkbox-8b" id="checkbox-8b" class="custom" /> 
+        <label for="checkbox-8b">u</label>    
+        </fieldset> 
+      </div> 
+ 
+      <div data-role="fieldcontain"> 
+          <fieldset data-role="controlgroup"> 
+            <legend>Choose a pet:</legend> 
+                <input type="radio" name="radio-choice-1" id="radio-choice-1b" value="choice-1" /> 
+                <label for="radio-choice-1b">Cat</label> 
+ 
+                <input type="radio" name="radio-choice-1" id="radio-choice-2b" value="choice-2"  /> 
+                <label for="radio-choice-2b">Dog</label> 
+ 
+                <input type="radio" name="radio-choice-1" id="radio-choice-3b" value="choice-3"  /> 
+                <label for="radio-choice-3b">Hamster</label> 
+ 
+                <input type="radio" name="radio-choice-1" id="radio-choice-4b" value="choice-4"  /> 
+                <label for="radio-choice-4b">Lizard</label> 
+          </fieldset> 
+      </div> 
+ 
+      <div data-role="fieldcontain"> 
+        <label for="select-choice-b" class="select">Choose shipping method:</label> 
+        <select name="select-choice-b" id="select-choice-b"> 
+          <option value="standard">Standard: 7 day</option> 
+          <option value="rush">Rush: 3 days</option> 
+          <option value="express">Express: next day</option> 
+          <option value="overnight">Overnight</option> 
+        </select> 
+      </div> 
+ 
+      
+      </div><!-- /body-b --> 
+ 
 
-  <script type="text/javascript">
-  Ext.setup({
-      icon: 'icon.png',
-      tabletStartupScreen: 'tablet_startup.png',
-      phoneStartupScreen: 'phone_startup.png',
-      glossOnIcon: false,
-      onReady: function() {
-          var tabpanel = new Ext.TabPanel({
-              tabBar: {
-                  dock: 'bottom',
-                  layout: {
-                      pack: 'center'
-                  }
-              },
-              fullscreen: true,
-              ui: 'light',
-              cardSwitchAnimation: {
-                  type: 'slide',
-                  cover: true
-              },
-              
-              defaults: {
-                  scroll: 'vertical'
-              },
-              items: [{
-                  title: 'About',
-                  html: '<h1>Bottom Tabs</h1><p>Docking tabs to the bottom will automatically change their style. The tabs below are type="light", though the standard type is dark. Badges (like the 4 &amp; Long title below) can be added by setting <code>badgeText</code> when creating a tab/card or by using <code>setBadge()</code> on the tab later.</p>',
-                  iconCls: 'info',
-                  cls: 'card1'
-              }, {
-                  title: 'Favorites',
-                  html: '<h1>Favorites Card</h1>',
-                  iconCls: 'favorites',
-                  cls: 'card2',
-                  badgeText: '4'
-              }, {
-                  title: 'Downloads',
-                  id: 'tab3',
-                  html: '<h1>Downloads Card</h1>',
-                  badgeText: 'Text can go here too, but it will be cut off if it is too long.',
-                  cls: 'card3',
-                  iconCls: 'download'
-              }, {
-                  title: 'Settings',
-                  html: '<h1>Settings Card</h1>',
-                  cls: 'card4',
-                  iconCls: 'settings'
-              }, {
-                  title: 'User',
-                  html: '<h1>User Card</h1>',
-                  cls: 'card5',
-                  iconCls: 'user'
-              }]
-          });
-      }
-  });
-  </script>
-  <style type="text/css" media="screen">
-     body {
-         background-color: #333;
-     }
-     .x-tabpanel > .x-panel-body .x-panel-body {
-         padding: 100px 0;
-         text-align: center;
-         font-size: 72px;
-         font-weight: bold;
-         color: rgba(0,0,0,.2);
-         text-shadow: rgba(255,255,255,.2) 0 1px 0;
-         padding: 100px 15%;
-     }
-     .x-phone .x-tabpanel > .x-panel-body .x-panel-body {
-         padding: 30px 20px;
-         font-size: 36px;
-     }
-     .x-phone p {
-         font-size: 16px;
-         line-height: 18px;
-     }
-     h1 {
-         font-weight: bold;
-     }
-     p {
-         font-size: 24px;
-         line-height: 30px;
-     }
-     .card1 .x-panel-body {
-         background-color: #ccc;
-     }
-     .card2 .x-panel-body {
-         background-color: #5E99CC;
-     }
-     .card3 .x-panel-body {
-         background-color: #759E60;
-     }
-     .card4 .x-panel-body {
-         background-color: #9C744F;
-     }
-     .card5 .x-panel-body {
-         background-color: #926D9C;
-     }
+      
+    </div><!--/content-primary--> 
+    
+    <div class="content-primary"> 
+      <nav> 
+        
+        <ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b"> 
+          <li data-role="list-divider">Components</li> 
+          <li><a href="docs/pages/index.html">Pages &amp; dialogs</a></li> 
+          <li><a href="docs/toolbars/index.html">Toolbars</a></li> 
+          <li><a href="docs/buttons/index.html">Buttons</a></li> 
+          <li><a href="docs/content/index.html">Content formatting</a></li> 
+          <li><a href="docs/forms/index.html">Form elements</a></li> 
+          <li><a href="docs/lists/index.html">List views</a></li> 
+          
+          <li data-role="list-divider">API</li> 
+          <li><a href="docs/api/globalconfig.html">Configuring defaults</a></li> 
+          <li><a href="docs/api/events.html">Events</a></li> 
+          <li><a href="docs/api/methods.html">Methods &amp; Utilities</a></li> 
+          <li><a href="docs/api/mediahelpers.html">Responsive Layout</a></li> 
+          <li><a href="docs/api/themes.html">Theme framework</a></li> 
+    
+        
+        </ul> 
+      </nav> 
+    </div> 
+ 
+    
+ 
+  </div> 
+  
+  <div data-role="footer" class="nav-glyphish-example"> 
+    <div data-role="navbar" data-grid="d" data-theme="e" class="nav-glyphish-example"> 
+      <ul> 
+        <li><a href="#" id="chat" data-icon="custom" data-theme="b">Chat</a></li> 
+        <li><a href="#" id="email" data-icon="custom" data-theme="b">Email</a></li> 
+        <li><a href="#" id="skull" data-icon="custom" data-theme="b">Danger</a></li> 
+        <li><a href="#" id="beer" data-icon="custom" data-theme="b">Beer</a></li> 
+        <li><a href="#" id="coffee" data-icon="custom" data-theme="b">Coffee</a></li> 
+      </ul> 
+    </div> 
+  </div> 
 
-  </style>
-</head>
-<body></body>
-</html>
+  <div data-role="footer" class="footer-docs" data-theme="c"> 
+      <p>&copy; 2011 The jQuery Project</p> 
+  </div>  
+  
+</div> 
+</body> 
+</html> 
