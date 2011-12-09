e5dcb71fe018a0a4de3ece56031e4d1e2294304c
Emerson Estrella <emerson.estrella@gmail.com>
2011-12-09 21:18:48 -0200

    1

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/_signin_form.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,28 +1,62 @@
-<?php use_helper('I18N') ?>
-<style>
-#loginMenu{
-  margin-top: 1em;
-}
-#loginMenu a{
-  color: black;
-  font-size: 1.4em;
-  text-decoration: none;
-}
-#loginMenu li{
-  list-style-type: none;
-  margin: 0px;
-  padding: 0.45em 0 0.25em 1.25em;
-  border-bottom: 1px solid white;
-}
-#loginMenu img{
-  margin-right: 0.7em;
-}
-</style>
-<ul id="loginMenu" data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b" id="_login"> 
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=twitter&type=oauth') ?>" rel="external" style="font-weight: lighter" title="Twitter"><img src="../images/twitter.png" alt="Twitter" class="ui-li-icon">Twitter</a></li> 
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=facebook&type=oauth') ?>" rel="external" style="font-weight: lighter" title="Facebook"><img src="../images/facebook.png" alt="Facebook" class="ui-li-icon">Facebook</a></li>
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=google&type=openid') ?>" rel="external" style="font-weight: lighter" title="Google"><img src="../images/google.png" alt="Google" class="ui-li-icon">Google</a></li> 
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=yahoo&type=openid') ?>" rel="external" style="font-weight: lighter" title="Yahoo!"><img src="../images/yahoo.png" alt="Yahoo!" class="ui-li-icon">Yahoo!</a></li> 
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=myopenid&type=openid') ?>" rel="external" style="font-weight: lighter" title="Linkedin"><img src="../images/linkedin.png" alt="Linkedin" class="ui-li-icon">Linkedin</a></li> 
-  <li><a href="<?php echo url_for('@default?module=webapp&action=login&service=myopenid&type=openid') ?>" rel="external" style="font-weight: lighter" title="OpenID"><img src="../images/hotmail.png" alt="OpenID" class="ui-li-icon">OpenID</a></li> 
-</ul> 
+<form id="gaia_loginform" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+
+<?php if ($form->hasGlobalErrors()): ?>
+  <tr>
+    <td colspan="4">
+      <ul class="error_list">
+        <?php foreach ($form->getGlobalErrors() as $name => $error): ?>
+          <li><?php echo $error ?></li>
+        <?php endforeach; ?>
+      </ul>
+    </td>
+  </tr>
+<?php endif; ?>
+
+  <table>
+    <tbody>
+      <?php //echo $form ?>
+     
+      <?php //echo $form['username']->renderRow() ?>
+      <?php //echo $form['password']->renderRow(array('class' => 'email')) ?>
+      <?php //echo $form['remember']->renderRow() ?>
+      
+        <table>
+          <tr>
+            <th><?php echo $form['username']->renderLabel() ?>:</th>
+            <td><?php echo $form['username'] ?></td>
+          </tr>
+          <tr>
+            <th><?php echo $form['password']->renderLabel() ?>:</th>
+            <td><?php echo $form['password'] ?></td>
+          </tr>
+          <tr>
+            <th><?php echo $form['remember']->renderLabel() ?>:</th>
+            <td><?php echo $form['remember'] ?></td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <input type="submit" value="<?php echo __('Entrar', null, 'sf_guard') ?>" />
+              <?php echo $form->renderHiddenFields() ?>
+            </td>
+          </tr>
+        </table>
+      
+    </tbody>
+    <tfoot>
+      <tr>
+        <td> </td>
+        <td>
+          
+          <?php $routes = $sf_context->getRouting()->getRoutes() ?>
+          <?php if (isset($routes['sf_guard_forgot_password'])): ?>
+            <a href="<?php echo url_for('@sf_guard_forgot_password') ?>"><?php echo __('Forgot your password?', null, 'sf_guard') ?></a>
+          <?php endif; ?>
+
+          <?php if (isset($routes['sf_guard_register'])): ?>
+            &nbsp; <a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'sf_guard') ?></a>
+          <?php endif; ?>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+</form>
