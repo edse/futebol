d4763f5822f215a3d28ff58083139c5a26277e62
Emerson Estrella <emerson.estrella@gmail.com>
2012-02-19 17:52:55 -0200

    1

--- web/update.php
+++ web/update.php
@@ -1,14 +1,16 @@
 <?php
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
-sfContext::createInstance($configuration)->dispatch();
-
 ini_set("upload_max_filesize", "1700M");
 ini_set("post_max_size", "1700M");
 ini_set("memory_limit", "1700M");
 ini_set("max_input_time", "3600");
 ini_set("max_execution_time", "3600");
 set_time_limit(0);
+
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+sfContext::createInstance($configuration)->dispatch();
+
+$this->setLayout(false);
 
 $ts = Doctrine_Query::create()
   ->select('t.*')
