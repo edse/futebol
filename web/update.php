f56ce07e13e62169f744c4ed31c996995dd3b471
Emerson Estrella <emerson.estrella@gmail.com>
2012-02-19 17:44:18 -0200

    1

--- web/update.php
+++ web/update.php
@@ -3,6 +3,13 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
 
+ini_set("upload_max_filesize", "1700M");
+ini_set("post_max_size", "1700M");
+ini_set("memory_limit", "1700M");
+ini_set("max_input_time", "3600");
+ini_set("max_execution_time", "3600");
+set_time_limit(0);
+
 $ts = Doctrine_Query::create()
   ->select('t.*')
   ->from('Tournament t')
